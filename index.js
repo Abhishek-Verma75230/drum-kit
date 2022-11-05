fa5d234e968e26bcc952bdd99aae296ac9945f14
Abhishek Verma <115094713+Abhishek-Verma75230@users.noreply.github.com>
2022-11-05 21:54:33 +0530

    version 1.1.0.0.1
    files added

---
+++ index.js
@@ -0,0 +1,67 @@
+// console.log("hello");
+const button = document.querySelectorAll(".set button");
+// console.log(typeof(button));
+// function fxn(){
+//     console.log("I am clicked");
+// }
+// Anonimous Function
+//  button[2].addEventListener("click",function(){
+//     console.log("yes");
+//  });
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function () {
+    var btn = this.innerHTML;
+       sound(btn);
+  });
+}
+document.addEventListener("keypress",function(event){
+   sound(event.key);
+})
+function sound(key)
+{
+  switch (key) {
+
+    case "W":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "A":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "S":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "D":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "J":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "K":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "L":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+      default:
+        console.log(this);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
